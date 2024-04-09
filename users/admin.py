75b3fb33bb9ee93807b00ffc4c90f104dab67dcc
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2024-04-09 08:33:23 +0100

    Updated User account model admin

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 from .models import UserAccount
 
 
-admin.site.register(UserAccount)
+@admin.register(UserAccount)
+class UserAccountModelAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        # If password is set, then set it using the set_password method
+        password = form.cleaned_data.get('password', None)
+        if password:
+            obj.set_password(password)
+        obj.save()
+        return None
